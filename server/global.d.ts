4b78f74e91bb06a16919fa7c00626f2080e236de
boomshakalakayo <dxk528@outlook.com>
2022-09-16 18:24:20 +0800

    update server

--- server/global.d.ts
+++ server/global.d.ts
@@ -1,14 +1,6 @@
 import PlayerMgr from "./playerMgr";
 import WorshipMgr from "./worshipMgr";
 
-// declare global {
-//     namespace NodeJS {
-//         interface Global {
-//             playerMgr: PlayerMgr;
-//             worshipMgr: WorshipMgr;
-//         }
-//     }
-// }
 declare global {
     var playerMgr: PlayerMgr;
     var worshipMgr: WorshipMgr;
